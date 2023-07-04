f1f429034fb6026eda59b391370b7d8392329148
Almadny <Almadny2u@gmail.com>
2023-07-04 23:43:55 +0000

    Fixed try catch statement to use if / else statement

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-unused-vars */
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const dataView = new DataView(buffer);
-  try {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  } else {
+    const dataView = new DataView(buffer);
     const int8view = new Int8Array(buffer);
     int8view[position] = value;
-  } catch (error) {
-    throw new Error('Position outside range');
+    return dataView;
   }
-  return dataView;
 }
